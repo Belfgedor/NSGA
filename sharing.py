15ac429e5e08f551863551a2dcf045bd31cbe993
Belfgedor <nachorubi@hotmail.com>
2016-11-06 23:24:15 +0100

    sharing

--- sharing.py
+++ sharing.py
@@ -3,4 +3,24 @@ import numpy as np
 #front_list, the list of individual selected in the front
 #dummyfitness the dummyfitness assigned to the front
 #Returns a list of fitness
+
+#Compute all the distances between each other
+#It is a list which contains in the i position the j distances for i
+#[[d11,d12,...d1n], [d21,d22.d]]
+#
+#
+def euclidean_distance(vector1,vector2):
+	result = 0
+	for i in xrange(len(vector1)):
+		result += np.square(vector2[i]-vector1[i])
+	return np.sqrt(result)
+def compute_distances():
+
+
+def niche_count(individual):
+	distances = compute_distances();
+	sum(share_function())
+
 def sharing( front_list, dummyfitness ):
+	return [dummyfitness / niche_count(individual) for individual in front_list]
+
