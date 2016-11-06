549d672772a3d2afeba2110086e8d4f23fc97998
Belfgedor <nachorubi@hotmail.com>
2016-11-06 20:17:16 +0100

    Tests implementation

--- tests_nsga.py
+++ tests_nsga.py
@@ -22,12 +22,12 @@ def t1_f1(population):
 def t1_g(population):
 	population_aux = population[:,1:]
 	sumatory = population_aux.sum(axis=1)
-	return 1 + 9 * sumatory / (n_design_var-1)
+	return 1 + 9.0 * sumatory / (n_design_var-1)
 def t1_h(f1,g):
 	return 1 - np.sqrt(f1 / g)
 
 def t1_f2(population):
-	return t1_g(x) * t1_h(t1_f1(population),t1_g(population))
+	return t1_g(population) * t1_h(t1_f1(population),t1_g(population))
 
 ##################################################################
 
@@ -49,12 +49,12 @@ def t2_f1(population):
 def t2_g(population):
 	population_aux = population[:,1:]
 	sumatory = population_aux.sum(axis=1)
-	return 1 + 9 * sumatory / (n_design_var-1)
+	return 1 + 9.0 * sumatory / (n_design_var-1)
 def t2_h(f1,g):
 	return 1 - np.square(f1 / g)
 
 def t2_f2(population):
-	return t2_g(x) * t2_h(t2_f1(population),t2_g(population))
+	return t2_g(population) * t2_h(t2_f1(population),t2_g(population))
 
 ##################################################################
 
@@ -78,12 +78,12 @@ def t3_f1(population):
 def t3_g(population):
 	population_aux = population[:,1:]
 	sumatory = population_aux.sum(axis=1)
-	return 1 + 9 * sumatory / (n_design_var-1)
+	return 1 + 9.0 * sumatory / (n_design_var-1)
 def t3_h(f1,g):
 	return 1 - np.sqrt(f1 / g) - (f1 / g) * np.sin(10*np.pi*f1)
 
 def t3_f2(population):
-	return t3_g(x) * t3_h(t3_f1(population),t3_g(population))
+	return t3_g(population) * t3_h(t3_f1(population),t3_g(population))
 
 ##################################################################
 
@@ -110,12 +110,12 @@ def t4_g(population):
 	population_aux = population[:,1:]
 	sumatory_inside = np.square(population_aux) - 10*np.cos(4*np.pi*population_aux)
 	sumatory = sumatory_inside.sum(axis=1)
-	return 1 + 10 * (n_design_var-1) + sumatory
+	return 1 + 10.0 * (n_design_var-1) + sumatory
 def t4_h(f1,g):
 	return 1 - np.sqrt(f1 / g)
 
 def t4_f2(population):
-	return t4_g(x) * t4_h(t4_f1(population),t4_g(population))
+	return t4_g(population) * t4_h(t4_f1(population),t4_g(population))
 
 ##################################################################
 
@@ -131,48 +131,63 @@ xi_type = binary
 number of decision variables: 11
 x1 = {0,1}³⁰ 
 x2...Xm = {0,1}⁵
-Pareto optimal front = g(x) = 1
-
+Pareto optimal front = g(x) = 10
+deceptive pareto optimal = g(x) = 11
+Population is matrix which contains the numbers of ones in each xi
 """
-def unitation(x1):
-	return x1.sum(axis=1)
-	
+def v_function(unitation):
+	result = []
+	for ones in unitation:
+		if(ones < 5):
+			result.append(ones+2)
+		elif(ones ==  5):
+			result.append(1)
+	return sum(result)
+
+
 def t5_f1(population):
-	return population[:,0] # return x1
+	return 1+population[:,0] # return x1
 
 def t5_g(population):
 	population_aux = population[:,1:]
-	sumatory = population_aux.sum(axis=1)
-	return 1 + 9 * sumatory / (n_design_var-1)
+	return np.apply_along_axis(v_function,axix=1, arr=population_aux)
+
 def t5_h(f1,g):
-	return 1 - np.sqrt(f1 / g)
+	return 1.0 / f1
 
 def t5_f2(population):
-	return t5_g(x) * t5_h(t5_f1(population),t5_g(population))
+	return t5_g(population) * t5_h(t5_f1(population),t5_g(population))
 
 ##################################################################
 
-##########################  TEST 1 ################################
+##########################  TEST 6 ################################
 """
 Test Features
 -------------------
+Search space is non-uniform, causes 2 difficulties: pareto-optimal solutions are distributed non-uniform along the global pareto front, the front
+is biases for solutions when f1(x) is close to one, the density of the solutions is lowest near the pareto-optimal front and highest away the front
 
-number of decision variables:
+NON Convex
+
+xi_type: real 
+number of decision variables: 10
 xi_interval = [0,1]
 Pareto optimal front = g(x) = 1
 
 """
 def t6_f1(population):
-	return population[:,0] # return x1
+	population_aux = population[:,0] # return x1
+	sinus_power = np.power(np.sin( 6 * np.pi * population_aux), 6)
+	return 1 - np.exp((-4.0) * population_aux) * sinus_power
 
 def t6_g(population):
 	population_aux = population[:,1:]
 	sumatory = population_aux.sum(axis=1)
-	return 1 + 9 * sumatory / (n_design_var-1)
+	return 1 + 9 * np.power( sumatory / (n_design_var-1) , 0.25)
 def t6_h(f1,g):
-	return 1 - np.sqrt(f1 / g)
+	return 1 - np.square(f1 / g)
 
 def t6_f2(population):
-	return t6_g(x) * t6_h(t6_f1(population),t6_g(population))
+	return t6_g(population) * t6_h(t6_f1(population),t6_g(population))
 
 ##################################################################
